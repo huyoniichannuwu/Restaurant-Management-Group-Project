fb7abea4b956a294ec6d4eb4c32133b031aae691
hoangda24312 <thanosligma942@gmail.com>
2026-01-17 19:53:45 +0700

    +them class manager

---
+++ RestaurantOrderManagement/RestaurantOrderManagement/Manager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Staff.h"
+#include "MenuItem.h"
+
+class Manager :public Staff
+{
+public:
+	float viewSalesReport(std::chrono::system_clock::time_point start_date, std::chrono::system_clock::time_point end_date);
+	void addMenuItem(MenuItem item);
+	void updateMenuItem(std::string item_id, std::string name, float price);
+	void removeMenuItem(std::string item_id);
+	void addStaff(Staff staff);
+	void updateStaff(std::string staff_id, Staff staff);
+	void removeStaff(std::string staff_id);
+};
