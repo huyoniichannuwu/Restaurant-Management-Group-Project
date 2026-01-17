2adc837093cd874a0668102776893aff89d472e6
hoangda24312 <thanosligma942@gmail.com>
2026-01-17 19:24:11 +0700

    +them invoice.h

--- RestaurantOrderManagement/RestaurantOrderManagement/Invoice.h
+++ RestaurantOrderManagement/RestaurantOrderManagement/Invoice.h
@@ -1,2 +1,25 @@
 #include "Order.h"
 #pragma once
+
+enum class PaymentStatus
+{
+	PENDING,
+	PAID,
+	REFUNDED
+};
+
+class Invoice
+{
+private:
+	int invoice_id;
+	int order_id;
+	std::chrono::system_clock::time_point issue_date;
+	PaymentStatus payment_status;
+	float total_price;
+public:
+	static Invoice generate(const Order& order);
+	float calculateTotal(Order order);
+	float calculateTotalSales();
+	void markPaid();
+	void markRefunded();
+};
