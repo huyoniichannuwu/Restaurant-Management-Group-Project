a06a35fac0ae89d3fc00fd9f8e5d07ff2f057d0a
hoangda24312 <thanosligma942@gmail.com>
2026-01-17 19:42:14 +0700

    +them class waiter

---
+++ RestaurantOrderManagement/RestaurantOrderManagement/Waiter.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Order.h"
+#include "Staff.h"
+
+class Waiter
+{
+	Order createOrder(int table_number, std::vector<std::vector<OrderItem>> items, std::string customer_name, std::string note);
+	std::vector<std::vector<Order>> viewActiveOrder(); //return list of order which status is not COMPLETED,PAID,CANCELLED
+};
